4a91dfe84567ccbdeea73b2825e3904930150b7f
Paul Beckwith <303dog@gmail.com>
2019-08-01 00:02:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,4 @@
-
+require.pry
 def my_hash()
   your_hash = {"gold" => "money","silver"=>"coin"}
   
@@ -9,9 +9,7 @@ end
 def shipping_manifest(the_manifest)
   the_manifest = {"whale_bone_corsets"=> 5,"porcelain_vases"=> 2,"oil_paintings"=> 3}
   puts shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+
 end
 
 def retrieval
